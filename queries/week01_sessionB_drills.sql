b0183dee13544bffd73944508441f699fc632061
sunnyakumaml <sunnyakuma.ml@gmail.com>
2026-03-17 14:55:44 +0100

    joined all tables together using inner join + group by + order by + aggregation

--- queries/week01_sessionB_drills.sql
+++ queries/week01_sessionB_drills.sql
@@ -39,3 +39,18 @@ ORDER BY oi.order_id;
 -- Write a query that shows: customer_id, full_name, total_spent
 -- joins customers -> orders -> order_items -> products, groups by customer, sorted by total_spent DESC
 
+SELECT * FROM customers;
+SELECT * FROM orders;
+SELECT * FROM order_items;
+SELECT * FROM products;
+
+SELECT o.customer_id, c.full_name, SUM(oi.qty * p.price) AS total_spent
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id 
+JOIN order_items oi
+	ON o.order_id = oi.order_id
+JOIN products p
+	ON oi.product_id = p.product_id
+GROUP BY o.order_id, c.full_name
+ORDER BY total_spent DESC;
