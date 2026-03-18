0960d761b2bf0f554c9f0b5888f7b29aa3603d4c
sunnyakumaml <sunnyakuma.ml@gmail.com>
2026-03-18 12:32:52 +0100

    drilling myself with some serious reps

--- queries/week01_homework_reps.sql
+++ queries/week01_homework_reps.sql
@@ -62,3 +62,63 @@ GROUP BY o.customer_id
 ORDER BY o.customer_id;
 
 -- Orders per customer (full_name + count)
+SELECT o.customer_id, count(o.customer_id) AS total_orders
+FROM orders o
+GROUP BY o.customer_id
+ORDER BY o.customer_id;
+
+-- Orders per customer (full_name + count)
+SELECT * FROM orders;
+SELECT * FROM customers;
+
+SELECT o.customer_id, c.full_name, count(o.customer_id) AS total_orders
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id
+GROUP BY o.customer_id, c.full_name
+ORDER BY o.customer_id;
+
+-- Total quantity sold per product_id
+SELECT * FROM order_items;
+
+SELECT oi.product_id, SUM(oi.qty) AS total_quantity
+FROM order_items oi
+GROUP BY oi.product_id
+ORDER BY oi.product_id
+
+-- Total quantity sold per product name
+SELECT * FROM products;
+
+SELECT p.name, sum(oi.qty) AS total_quantity
+FROM order_items oi
+JOIN products p
+	ON oi.product_id = p.product_id
+GROUP BY p.name
+ORDER BY p.name
+
+-- Total revenue per order_id
+SELECT * FROM products;
+SELECT * FROM order_items;
+
+SELECT oi.order_id, SUM(oi.qty * p.price) AS total_revenue
+FROM order_items oi
+JOIN products p
+    ON oi.product_id = p.product_id 
+GROUP BY oi.order_id
+ORDER BY oi.order_id
+
+-- Total revenue per customer name
+SELECT * FROM orders;
+
+SELECT c.full_name, SUM(oi.qty * p.price) AS total_revenue
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+JOIN order_items oi
+	ON o.order_id = oi.order_id
+JOIN products p
+    ON oi.product_id = p.product_id
+GROUP BY c.full_name
+ORDER BY c.full_name;
+
+-- Top 1 highest spending customer
