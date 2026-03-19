4afc7da9fe460cadfd2adae7381753cd840823e8
sunnyakumaml <sunnyakuma.ml@gmail.com>
2026-03-19 18:14:08 +0100

    added week2 reading

---
+++ reading/week02_reading.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE dvdrental;
+SELECT current_database();
+
+-- DiSTINCT syntax - SELECT DISTINCT col FROM table;
+SELECT DISTINCT rating
+FROM film;
+
+SELECT DISTINCT city
+FROM city
+LIMIT 6;
+
+-- IN syntax - WHERE col IN (value1, value2, value3)
+SELECT title, rating
+FROM film
+WHERE rating IN ('G', 'PG')
+LIMIT 6;
+
+SELECT first_name, last_name
+FROM actor
+WHERE last_name IN ('Chase', 'Davis')
+LIMIT 5;
+
+-- BETWEEN syntax - WHERE col BETWEEN low AND high;
+
+SELECT title, rating, rental_rate 
+FROM film
+WHERE rental_rate BETWEEN 2.99 AND 4.99
+LIMIT 20;
+
+SELECT payment_id, amount
+FROM payment
+WHERE amount BETWEEN 1 AND 3
+LIMIT 10;
+
+-- LIKE syntax - WHERE text_col AS 'A%'(starts with A), WHERE text_col AS %A% (contains A)
+SELECT title
+FROM film
+WHERE title LIKE 'A%'
+LIMIT 10;
+
+SELECT first_name, last_name
+FROM actor
+WHERE last_name LIKE '%son%'
+LIMIT 10;
+
+-- NULL syntax - WHERE col IS NULL, WHERE col IS NOT NULL
+SELECT address_id, address2
+FROM address
+WHERE address2 IS NULL
+LIMIT 10;
+
+SELECT address_id, address2
+FROM address
+WHERE address2 IS NOT NULL 
+LIMIT 10;
+
+-- OFFSET syntax - ORRDER BY col LIMIT n OFFSET k;
+SELECT film_id, title
+FROM film
+ORDER BY film_id
+LIMIT 10 OFFSET 0;
+
+SELECT film_id, title
+FROM film
+ORDER BY film_id
+LIMIT 10 OFFSET 10;
